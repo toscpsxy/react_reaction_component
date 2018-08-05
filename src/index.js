dfde56f88aa1ed3d7ad25923024ede7c7633dc53
toscpsxy <42105400+toscpsxy@users.noreply.github.com>
2018-08-05 12:34:55 +0800

    - setState
    - added setState example

--- src/index.js
+++ src/index.js
@@ -6,12 +6,20 @@ import "./styles.css";
 
 function App() {
   return (
-    <Component
-      initialState={{ name: "Brown" }}
-    >
-      {({ state }) => {
+    <Component initialState={{ name: "Brown" }}>
+      {({ state, setState }) => {
         return (
-          <h1>Hello {state.name}</h1>
+          <div>
+            <label>
+              Name :
+              <input
+                type="text"
+                autocomplete="name"
+                onInput={e => setState({ name: e.target.value })}
+              />
+            </label>
+            <h1>Hello {state.name}</h1>
+          </div>
         );
       }}
     </Component>
